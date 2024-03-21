a8dd9c6312635cfaa7539bf47ad9beffad9334bf
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-03-22 00:36:25 +0700

    add: add task

---
+++ lib/services/AddTask.dart
@@ -0,0 +1,29 @@
+import 'package:intl/intl.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'package:todo/models/congviec.dart';
+import 'package:todo/Database/DBHelper.dart';
+
+class AddTask {
+  static Future<void> addTask(CongViec task) async {
+    try {
+      final Database database = await DBHelper.initializeDatabase();
+      final DateFormat dateFormat = DateFormat('yyyy-MM-dd');
+
+      await database.insert(
+        DBHelper.taskTable,
+        {
+          'name': task.name,
+          'description': task.description,
+          'date': dateFormat.format(task.date),
+          'time': task.time,
+          'priority': task.priority,
+        },
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    } catch (e) {
+      print('Error adding task: $e');
+    }
+  }
+}
+ 
