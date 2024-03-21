1c5af094c85e41bd8f110b65380fe2516135fef7
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-03-21 19:29:42 +0700

    add: SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/pages/TodoList.dart';
 
 import 'package:todo/theme/provider.dart';
@@ -9,7 +10,7 @@ import 'package:todo/Database/Dbhelper.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DBHelper.initializeDatabase();
-
+  await SharedPreferences.getInstance(); // save viewmode
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: themeProvider.themeData,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'TODO',
       home: const TodoSample(),
     );
   }
