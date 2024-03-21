9e2a7240b504bbfa0f8b621ae6adbe075072cc62
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-03-21 17:00:37 +0700

    add: sharedPreferences(save viewmode)

--- lib/theme/provider.dart
+++ lib/theme/provider.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/theme/theme.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeData _themeData = lightmode;
+  static const String _themePreferenceKey = 'themePreference';
 
   ThemeData get themeData => _themeData;
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? themePreference = prefs.getInt(_themePreferenceKey);
+    if (themePreference != null) {
+      _themeData = themePreference == 0 ? lightmode : darkmode;
+    }
+    notifyListeners();
+  }
+
+  void _saveThemePreference(int themePreference) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt(_themePreferenceKey, themePreference);
+  }
+
   set themeData(ThemeData themeData) {
     _themeData = themeData;
+    _saveThemePreference(themeData == lightmode ? 0 : 1);
     notifyListeners();
   }
 
   void toggleTheme() {
-    themeData = (_themeData == lightmode) ? darkmode : lightmode;
+    if (_themeData == lightmode) {
+      _themeData = darkmode;
+    } else {
+      _themeData = lightmode;
+    }
+    _saveThemePreference(_themeData == lightmode ? 0 : 1);
     notifyListeners();
   }
 }
