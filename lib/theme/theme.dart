f1063b45663311a7d84123a076b8ba38c8430a85
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-04-03 20:27:43 +0700

    update : widget

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 ThemeData lightmode = ThemeData(
   brightness: Brightness.light,
   colorScheme: const ColorScheme.light(
-    background: Colors.white,
+    surface: Colors.white,
   ),
   primaryColor: Colors.grey,
 );
 ThemeData darkmode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: const ColorScheme.dark(
-    background: Colors.black87,
+    surface: Colors.black87,
   ),
   primaryColor: Colors.black,
 );
